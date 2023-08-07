ca1fae33758650b02c3142be3ba5a8a5ac9ce978
Masaya Morita <47968855+Masaya0606@users.noreply.github.com>
2023-08-07 11:54:31 +0900

    Update Fitering_SNPs_with_Plink.sh

--- Fitering_SNPs_with_Plink.sh
+++ Fitering_SNPs_with_Plink.sh
@@ -1,1 +1,23 @@
+##obtain bed files from vcf file
 
+#!/bin/sh
+#$ -S /bin/bash
+#$ -cwd
+#$ -l s_vmem=156G
+#$ -l mem_req=156G
+#$ -l d_rt=480:00:00
+#$ -l s_rt=480:00:00
+
+singularity exec /usr/local/biotools/p/plink\:1.90b6.21--h779adbc_1 plink --vcf Ahya.snps.vcf  --make-bed --double-id --aec --out Ahya
+
+##filtering with bed files
+
+#!/bin/sh
+#$ -S /bin/bash
+#$ -cwd
+#$ -l s_vmem=156G
+#$ -l mem_req=156G
+#$ -l d_rt=480:00:00
+#$ -l s_rt=480:00:00
+
+singularity exec /usr/local/biotools/p/plink\:1.90b6.21--h779adbc_1 plink --bfile Ahyacinthus --geno 0.05 --maf 0.05 --hwe 0.000001 --make-bed --allow-extra-chr --out Ahya_maf_hwe_geno005
