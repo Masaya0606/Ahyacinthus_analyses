6f29eb01593390f8cf5c096ea36b5f3c8fbfbc8e
Masaya Morita <47968855+Masaya0606@users.noreply.github.com>
2023-08-07 13:55:35 +0900

    Update Fst_selection.sh

--- Fst_selection.sh
+++ Fst_selection.sh
@@ -1,3 +1,5 @@
+##following commands should be done with two references A.hyacinthus and A.cyhtehrea each
+
 ##select scafold and nucleoposition of Fst sites (>0.8)
 
 awk '$3>0.8' sub_bif_filtered.weir.fst|grep -v "nan"|awk '{print $1,$2}'|sed "s/ /_/g"|sort|uniq|grep -v "CHROM" >sub_bif_filtered.weir.fst_08.sites
@@ -9,7 +11,7 @@ comm -1 -2 sub_bif_filtered.weir.fst_08.sites sub_hya_filtered.weir.fst_08.sites
 comm -1 -2 sub_bif_sub_hya_filtered.weir.fst_08.sites sub_cyt_filtered.weir.fst_08.sites>common.sites
 
 ## select only A.subulata and A.cytherea specific sites
-diff sub_bif_filtered.weir.fst_08.sites sub_cyt_filtered.weir.fst_08.sites|grep ">"|sed "s/> //g" >sub_cyt_specific_ag_sub_bif
-diff sub_hya_filtered.weir.fst_08.sites sub_cyt_specific_ag_sub_bif|grep ">"|sed "s/> //g" >sub_cyt_specific
+diff sub_bif_filtered.weir.fst_08.sites sub_cyt_filtered.weir.fst_08.sites|grep ">"|sed "s/_/\t/g" > //g" >sub_cyt_specific_ag_sub_bif
+diff sub_hya_filtered.weir.fst_08.sites sub_cyt_specific_ag_sub_bif|grep ">"|sed "s/> //g"|sed "s/_/\t/g" >sub_cyt_specific
 
 
